f493604cef35b7831ba40647fcdd561e8ac6b090
tuyetngocdhcn4a <huynhthituyetngocdhcn4a@gmail.com>
2020-12-05 20:28:06 +0700

    cai-dat-boostrap: cai dat js va boostrap 4

---
+++ index.php
@@ -0,0 +1,19 @@
+<!-- HEADER -->
+<?php
+include 'src/custom-header.php';
+?>
+
+
+<!-- BODY -->
+
+<body>
+    <p class="text-left">Left aligned text on all viewport sizes.</p>
+    <p class="text-center">Center aligned text on all viewport sizes.</p>
+    <p class="text-right">Right aligned text on all viewport sizes.</p>
+</body>
+
+
+<!-- FOOTER -->
+<?php
+include 'src/custom-footer.php';
+?>
